6ebdc4cce2ba0b692236c463d8ecf2598bb4d4c5
DLUTLiuFengyi <545184554@qq.com>
2019-12-19 11:16:31 +0800

    Hystrix和Zuul

---
+++ fdums-gateway-zuul/src/main/java/study/FdumsGatewayZuulApplication.java
@@ -0,0 +1,14 @@
+package study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class FdumsGatewayZuulApplication {
+
+public static void main(String[] args) {
+    SpringApplication.run(FdumsGatewayZuulApplication.class, args);
+}
+}
