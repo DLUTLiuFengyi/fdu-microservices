8c5384d2eb4d5e808e54dc7e6ccb1959e4ff28aa
DLUTLiuFengyi <545184554@qq.com>
2020-03-02 22:09:16 +0800

    最后的补充

---
+++ fdums-provider-user-compose/src/main/java/study/controller/UserController.java
@@ -0,0 +1,26 @@
+package study.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.entity.User;
+import study.repository.UserRepository;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/{id}")
+    public User findById(@PathVariable Long id) {
+        User findOne = this.userRepository.findOne(id);
+        return findOne;
+    }
+
+    @GetMapping("/get")
+    public User get(User user){
+        return this.userRepository.findOne(user.getId());
+    }
+}
